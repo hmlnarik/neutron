1fe670c0cc4f3fe660be6d4eae578868baff3d7b
Hynek Mlnarik <hmlnarik@redhat.com>
2016-01-27 09:15:37 +0100

    Enable neutron-debug process multiple --config-file and --config-dir options
    Contrary to neutron server and agents, neutron-debug does not support
    processing multiple configuration file. This patch fixes this difference,
    allowing neutron-debug for accepting --config-file and --config-dir
    options in the same way as neutron and agents.
    
    Change-Id: Ia1c59ef1db2149babdbe9ee43274d6b8e89226f0

--- neutron/debug/shell.py
+++ neutron/debug/shell.py
@@ -13,6 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import argparse
 import sys
 
 from oslo_config import cfg
@@ -22,6 +23,7 @@ from neutron._i18n import _
 from neutron.agent.common import config
 from neutron.agent.common import utils
 from neutron.agent.linux import interface
+from neutron.common import config as common_config
 from neutron.debug import debug_agent
 from neutronclient.common import exceptions as exc
 from neutronclient import shell
@@ -50,31 +52,50 @@ class NeutronDebugShell(shell.NeutronShell):
         for k, v in COMMANDS[api_version].items():
             self.command_manager.add_command(k, v)
 
+    class ExtendListAction(argparse.Action):
+        def __call__(self, parser, args, values, option_string=None):
+            arg_value = getattr(args, self.dest, [])
+            arg_value.extend([option_string, values])
+            setattr(args, self.dest, arg_value)
+
     def build_option_parser(self, description, version):
         parser = super(NeutronDebugShell, self).build_option_parser(
             description, version)
-        default = (
-            shell.env('NEUTRON_TEST_CONFIG_FILE') or
-            shell.env('QUANTUM_TEST_CONFIG_FILE')
-        )
+        config_file = (shell.env('NEUTRON_TEST_CONFIG_FILE') or
+                       shell.env('QUANTUM_TEST_CONFIG_FILE'))
+        default = ["--config-file", config_file] if config_file else []
+
         parser.add_argument(
             '--config-file',
             default=default,
+            dest="config_opts",
+            action=NeutronDebugShell.ExtendListAction,
+            metavar='PATH',
             help=_('Config file for interface driver '
                    '(You may also use l3_agent.ini)'))
+        parser.add_argument(
+            '--config-dir',
+            default=default,
+            dest="config_opts",
+            action=NeutronDebugShell.ExtendListAction,
+            metavar='DIR',
+            help=_('Directories with config file for interface driver'))
         return parser
+
+    def register_opts(self, conf):
+        conf.register_opts(interface.OPTS)
+        conf.register_opts(config.EXT_NET_BRIDGE_OPTS)
+        config.register_interface_driver_opts_helper(conf)
 
     def initialize_app(self, argv):
         super(NeutronDebugShell, self).initialize_app(argv)
-        if not self.options.config_file:
+        if not self.options.config_opts:
             raise exc.CommandError(
                 _("You must provide a config file for bridge -"
                   " either --config-file or env[NEUTRON_TEST_CONFIG_FILE]"))
         client = self.client_manager.neutron
-        cfg.CONF.register_opts(interface.OPTS)
-        cfg.CONF.register_opts(config.EXT_NET_BRIDGE_OPTS)
-        config.register_interface_driver_opts_helper(cfg.CONF)
-        cfg.CONF(['--config-file', self.options.config_file])
+        self.register_opts(cfg.CONF)
+        common_config.init(self.options.config_opts)
         config.setup_logging()
         driver = utils.load_interface_driver(cfg.CONF)
         self.debug_agent = debug_agent.NeutronDebugAgent(cfg.CONF,
